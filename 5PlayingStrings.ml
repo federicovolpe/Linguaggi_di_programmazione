f76af7f25e5eb03f49d6cceac88407f70158a534
Federico <91740549+federicovolpe@users.noreply.github.com>
2023-10-04 21:30:49 +0200

    esercizio 5 finito e funzionante

--- 5PlayingStrings.ml
+++ 5PlayingStrings.ml
@@ -38,8 +38,8 @@ let rec subStringUnary (l1: char list) (l2: char list): char list =
       match l1 with                                                                 
       | h :: tail when h = c -> tail                                                
       | h :: tail -> h :: subchar tail c                                                             
-      | [] -> []                                                                                     
-    in                                                                                                         
+      | [] -> []                                                                                                   
+    in                                                                                                             
   match l2 with                                                                                                          
   | h :: tail when List.mem h l1 -> subStringUnary (subchar l1 h) tail                                                   
   | h :: tail -> subStringUnary l1 tail                                                                                  
@@ -48,32 +48,52 @@ let rec subStringUnary (l1: char list) (l2: char list): char list =
                                                                                                                          
 (**caso in cui si vogliono togliere tutte le ricorrenze dei caratteri della stringa 2 dalla stringa 1*)                  
 let rec subString (l1: char list) (l2: char list): char list =                                                                                                                    
-  match l1 with                                                                                                          
-  | h :: tail when List.mem h l2 -> (* se la testa e' contenuta nella seconda lista processo la coda *)
-                                    subString tail l2                                                   
-  | h :: tail -> h :: subString tail l2                                                                                  
-  | [] -> []                                                                                                             
-;;                                                                                                                       
-                                                                                                                         
+  match l1 with                                                                                                                                   
+  | h :: tail when List.mem h l2 -> (* se la testa e' contenuta nella seconda lista processo la coda *)                                           
+                                    subString tail l2                                                                                             
+  | h :: tail -> h :: subString tail l2                                                                                                           
+  | [] -> []                                                                                                                                      
+;;                                                                                                                                                
+                                                                                                                                                  
 let compose_string (l: char list): string =                                                                                                         
     let rec aux (list: char list) (res: string) =                                                                                                                                      
       match list with                                                                                                                                                                  
-      | [] ->                                                                                                            
+      | [] ->                                                                                                                                     
               res                                                                                                                                                                        
-      | h :: t ->                                                                                                        
-              (* da carattere a stringa e lo concateno al risultato *)
+      | h :: t ->                                                                                                                                 
+              (* da carattere a stringa e lo concateno al risultato *)                                                                            
               aux t (res ^ String.make 1 h)                                                                                                                                           
   in                                                                                                                                                                                   
   aux l ""                                                                                                                                                                             
 ;;                                                                                                                                                                                     
                                                                                                                                                                                        
-let (-) (s1: string) (s2: string) : string =                                                                                                                                           
-  (*decomposizione delle stringhe in liste su cui fare pattern matching facilmente*)                                                                                                                     
-  let l1 = String.fold_left (fun acc c -> acc @ [c]) [] s1 in                                                                                                                          
-  let l2 = String.fold_left (fun acc c -> acc @ [c]) [] s2 in                                                                                                                          
-                                                                                                                                                                                       
-  (* composizione della lista risultante *)                                                                                                                                            
-  compose_string (subString l1 l2)                                                                                                                                                     
-;;                                                                                                                                                                                     
-                                                                                                                                                                                       
-                                                                                                                                                                                       
+let (-) (s1: string) (s2: string) : string =                                                                                                                                                                                                           
+  (*decomposizione delle stringhe in liste su cui fare pattern matching facilmente*)                                                                                                                                                                   
+  let l1 = String.fold_left (fun acc c -> acc @ [c]) [] s1 in                                                                                                                                                                                          
+  let l2 = String.fold_left (fun acc c -> acc @ [c]) [] s2 in                                                                                                                                                                                          
+                                                                                                                                                                                                                                                       
+  (* composizione della lista risultante *)                                                                                                                                                                                                            
+  compose_string (subString l1 l2)                                                                                                                                                                                                                     
+;;                                                                                                                                                                                                                                                     
+                                                                                                                                                                                                                                                       
+(** -------------------------- anagram --------------------------*)              
+(** date due stringhe stabilisce se sono anagrammi *)                                
+let are_anagrams (s1: string) (s2: string) : bool =                                                                                                 
+  let l1 = List.sort                           (*scomposizione e sorting in lista*)                                                                                                    
+            (fun c1 c2 -> Char.compare c1 c2)                                                                                                       
+            (String.fold_left (fun acc c -> acc @ [c]) [] s1) in                                                                                    
+  let l2 = List.sort                                                                                                                                
+            (fun c1 c2 -> Char.compare c1 c2)                                                                                                       
+            (String.fold_left (fun acc c -> acc @ [c]) [] s2) in                                                                                    
+                                                                                                                                                    
+  List.equal (fun c1 c2 -> c1 = c2) l1 l2      (*comparazione delle due liste sortate*)                                                                                                       
+;;                                                                                                                                                                                                
+let rec anagram (s: string) (l: string list) : bool =                                                                                                                                             
+  match l with                                                                                                                                                                                    
+  | h :: t when are_anagrams h s -> true                                                                                                                                                          
+  | h :: t -> anagram s t                                                                                                                                                                         
+  | [] -> false                                                                                                                                                                                   
+;;                                                                                                                                                                                                
+                                                                                                                                                                                                  
+(*testing... *)                                                                                                                                                                                                 
+anagram "nace" ["ociredef";"cane";"pippo";"pasta";"pluto";"paperino";"qui";"quo";"qua"];;                                                                                                     

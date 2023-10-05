13ed05db3e5af7894694ecfe1a11005bbfa0866f
Federico <91740549+federicovolpe@users.noreply.github.com>
2023-10-05 12:35:41 +0200

    chemistry aggiornato come lo ha fatto il prof e cancellata un file errato

--- 1Chemistry.ml
+++ 1Chemistry.ml
@@ -1,37 +1,51 @@
-let alkaline_earth_metals = ("beryllium", 4) :: ("magnesium", 12) :: ("calcium", 20) ::                              
-                              ("strontium", 38) :: ("barium", 56) :: ("radium", 88) :: ("polonium", 77)::[]  ;;                            
-                                                                                                                
-let highestAtomicNumber(list) =                                                                                 
-    let rec aux(list, highest) =                                                                                
-        match list with                                                                                         
-        | [] -> highest                                                                                         
-        | (metal, n) :: t -> if n > (snd highest) then                                                          
-                                aux(t, (metal, n))                                                              
-                            else                                                                                
-                                aux(t, highest)                                                                                                                                      
-    in                                                                                                                                                                               
-aux(list, ("", 0)) ;;                                                                                                                                                                
-                                                                                                                                                                                     
-(** sorting funciton  *)                                                                                                                                                             
-let criteria a b = if snd a > snd b then 1                                                                                                                                           
-                     else if snd a == snd b then 0                                                                                                                                   
-                     else -1;;                                                                                                                                                       
-                                                                                                                                                                                     
-List.sort (criteria) alkaline_earth_metals;;                                                                
-                                                                                                            
-let gasses = ("helium", 2)::("neon", 10)::("argon", 18)::("krypton", 36)::("xenon", 54)::("radon", 86)::[];;
-                                                                                                         
-let sortNome a:('a * 'b) b:('a * 'b) =                                                                                       
-    let rec strings x y =                                                                                
-        match (x, y) with                                                                                
-        | (h1::t1 , h2::t2) -> if criteria h1 h2 == 0 then strings t1 t2                                 
-                               else criteria h1 h2                                                       
-        | ([], z) -> -1                                                                                  
-        | (z, []) -> 1                                                                                         
-    in                                                                                                   
-    strings (fst a) (fst b);;                                                                            
-                                                                                                         
-let x = gasses @ alkaline_earth_metals;;                                                                 
-                                                                                                         
-List.merge sort;;                              
+(**esercizio agggiunto pari pari a come lo ha svolto il professore
+    con la sola aggiunta di commenti per la spiegazione *)
+                                            
+let alkaline_earth_metals = ("beryllium", 4)::("magnesium", 12)::("calcium", 20)::("strontium", 38)::("barium", 56)::("radium", 88)::[] ;;
+                                                                       
+let noble_gases = [("helium", 2); ("neon", 10); ("argon", 18); ("krypton", 36); ("xenon", 54); ("radon", 86)] ;;
+                                            
+(** funzione per il sorting delle tuple *)
+let (>:) a b =       
+    (snd a) - (snd b)
+;;                                     
+let max a b =            
+    if (a >: b >= 0)     
+        then a           
+    else b               
+;;                                            
+                                                                       
+let heaviest lst =                                                     
+  List.fold_left max (List.hd lst) (List.tl lst) ;;                    
+                                                                       
+let sort_ascending lst =                      
+    List.sort (>:) lst                        
+;;                                            
+                                              
+let merge_elements metals gases =             
+    (*sorting delle due liste *)            
+  let sorted_metals = sort_ascending metals and 
+      sorted_gases = sort_ascending gases     
+in                                            
+List.merge (>:) sorted_metals sorted_gases    
+;;                                            
+                                              
+let print_list f lst =                        
+  let rec print_list = function               
+    []     -> ()                              
+  | hd::[] -> f hd                            
+  | hd::tl -> f hd ; print_string ", " ; print_list tl 
+in print_string "[" ; print_list lst ; print_string "]\n" ;;
+                                            
+let print_pair a =                          
+  Printf.printf "(%s, %d)" (fst a) (snd a) ;;
+                               
+let main() =                   
+  let m = (heaviest alkaline_earth_metals) and
+      sorted_aem = sort_ascending alkaline_earth_metals in
+    Printf.printf "The heaviest element is «%s» with «%2d» as atomic number.\n" (fst m) (snd m) ;
+    print_list print_pair sorted_aem ;
+    print_list print_pair (merge_elements alkaline_earth_metals noble_gases) ;;
+         
+main() ;;                              
                                                    

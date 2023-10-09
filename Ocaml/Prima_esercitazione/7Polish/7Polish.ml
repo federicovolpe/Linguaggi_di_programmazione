be6ffeee5a91b636983cf38f4a61e47d484ea120
Federico <91740549+federicovolpe@users.noreply.github.com>
2023-10-09 09:23:07 +0200

    inizializzato 2Playing algebra

--- Ocaml/Prima_esercitazione/7Polish/7Polish.ml
+++ Ocaml/Prima_esercitazione/7Polish/7Polish.ml
@@ -1,25 +1,43 @@
-(*Esercizio non ancora terminato *)                                           
-module Polish = struct                                    
-    (*                                                                                  
-    type operation =                                                                  
-      | "+" -> fun x y -> x + y                                                                           
-      | "-" -> fun x y -> x - y                                                       
-      | "*" -> fun x y -> x * y                                                       
-      | "/" -> fun x y -> x / y                                                       
-                                                                                      
-    type number = int ;;                                                               
-                                                                                      
-    type expr = (number | operation) Stack.t                                                   
-                                                                                      
-    (**presa una stringa di una espressione la trasforma in una espressione*)         
-    let expr_of (s: string): expr = function                                          
-    | number -> (*adds the number to the resulting expr*)                             
-    | operation -> (*adds the operator to the expr*)                                                          
-                                                                                      
-    (**function that evaluates the function created*)                                 
-    let eval (e: expr): int =                                                         
-      match e with                                                                    
-      | Stack vuoto ->                                                                
-      | operation :: resto ->                                                         
-     *)                                                                                 
-end;;                                                                                 
+(*L'esercizio viene risolto con l'utilizzo dello Shunting Yard algorithm per la traduzione
+   da notazione classica a polish *)         
+                                   
+type expr =                                                                             
+  |Num of int                                                                           
+  |Add of expr * expr                                                                   
+  |Sub of expr * expr                                                                   
+  |Mul of expr * expr                                                                   
+  |Div of expr * expr                                                                   
+  |Pow of expr * expr                                                                   
+;;                                                                                      
+                                                                                        
+(**funzione che trasforma la stringa data in input in una espressione in forma polish *)
+let exp s =                                                                             
+    let rec expr_of_string (s: string list): expr =                                     
+      match s with                                                                     
+      | [] -> failwith "Empty expression string"                                        
+      | "(" :: e :: ")" :: t -> expr_of_string (e :: t)                                       
+      | [x] -> Num (int_of_string x)                                                    
+      | h :: t ->                                                                       
+              match h with                                                              
+                | "+" -> Add (expr_of_string t, expr_of_string [])                      
+                | "-" -> Sub (expr_of_string t, expr_of_string [])                      
+                | "*" -> Mul (expr_of_string t, expr_of_string [])                      
+                | "/" -> Div (expr_of_string t, expr_of_string [])      
+                | "^" -> Pow (expr_of_string t, expr_of_string [])      
+                | _ -> failwith ("Unknown operator " ^ h)               
+  in                                                                    
+  expr_of_string (String.split_on_char ' ' s)                           
+;;                                                                      
+                                                                        
+let rec eval (exp: expr): int =                                         
+  match exp with                                                        
+  | Num n -> n                                                          
+  | Add (e1, e2) -> eval e1 + eval e2                                   
+  | Sub (e1, e2) -> eval e1 - eval e2                                   
+  | Mul (e1, e2) -> eval e1 * eval e2                                   
+  | Div (e1, e2) -> eval e1 / eval e2                                   
+  | Pow (e1, e2) -> int_of_float (float_of_int (eval e1) ** float_of_int (eval e2))                  
+;;                                                                      
+                                                                        
+(*testing*)                             
+let espressione = exp "( 3 + 4 ) * 5" ;;
---
+++ Ocaml/Seconda_esercitazione/2PlayingAlgebra/2PlayingAlgebra.ml
@@ -0,0 +1,38 @@
+module Monoid = struct    
+  (* additive operation *)
+  let add                 
+  ;;                      
+  (* multiplicative operation *)
+  let mul                 
+  ;;                      
+  (* identity operand *)  
+  let i                   
+  ;;                      
+                          
+end                       
+                          
+module Group = struct    
+  (* additive operation *)
+  let add                 
+  ;;                      
+  (* multiplicative operation *)
+  let mul                 
+  ;;                      
+  (* identity operand *)  
+  let i                   
+  ;;                      
+                          
+end              
+                          
+module Ring = struct    
+  (* additive operation *)
+  let add                 
+  ;;                      
+  (* multiplicative operation *)
+  let mul                 
+  ;;                      
+  (* identity operand *)  
+  let i                   
+  ;;                      
+                          
+end               

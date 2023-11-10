154d3843183ad723aa33bf904114fd13494c30af
Federico <91740549+federicovolpe@users.noreply.github.com>
2023-11-10 16:23:32 +0100

    esercizio server.erl terminato

--- Erlang/Prima_esercitazione/es5/server.erl
+++ Erlang/Prima_esercitazione/es5/server.erl
@@ -1,35 +1,32 @@
--module(server).
--export([create/0,loop/1]).
-
-create() -> %funzione che crea il processo server
-    register(server, spawn(?MODULE, loop,[[0,0,0,0,0]])).
-
-    aggiornalista(Lista, Nuova = [], I){
-        
-    }
-
-loop(Contatore) ->
-    receive
-        dummy0 -> %restituisco ok
-            NewContatore = [lists:nth(0, Contatore)+1,lists:nth(1, Contatore),lists:nth(2, Contatore),lists:nth(3, Contatore),lists:nth(4, Contatore)],
-            loop(NewContatore);
-        dummy1 -> %restituisco ok
-            NewContatore = lists:nth(1, Contatore),lists:nth(1, Contatore)+1,lists:nth(2, Contatore),lists:nth(3, Contatore),lists:nth(4, Contatore),
-            loop(NewContatore);
-        dummy2 -> 
-            NewContatore = [lists:nth(0, Contatore),lists:nth(1, Contatore),lists:nth(2, Contatore)+1,lists:nth(3, Contatore),lists:nth(4, Contatore)],
-            loop(NewContatore);
-        dummy3 -> 
-            NewContatore = [lists:nth(0, Contatore),lists:nth(1, Contatore),lists:nth(2, Contatore),lists:nth(3, Contatore)+1,lists:nth(4, Contatore)],
-            loop(NewContatore);
-        dummy4 -> 
-            NewContatore = [lists:nth(0, Contatore),lists:nth(1, Contatore),lists:nth(2, Contatore),lists:nth(3, Contatore),lists:nth(4, Contatore)+1],
-            loop(NewContatore);
-        tot -> 
-            io:format("~p~n",[lists:sum(Contatore)]),
-            loop(Contatore); %ritorna il numero di chiamate che sono state fatte
-        kill -> unregister(server),exit(normal);
-        Other -> 
-            io:format("~pnon è una funzione supportata~n",[Other]),
-        loop(Contatore)
-    end.
+% esercizio terminato
+-module(server).                                    
+-export([start/0, loop/1]).                         
+                                                    
+start() -> %funzione che crea il processo server   
+    register(server, spawn(?MODULE, loop,[[]])).
+                                                    
+                                                    
+loop(Servizi) ->                                    
+    receive                                         
+        d0 ->                                  
+            loop(aggiungi(dummy0, Servizi));                               
+        d1 ->                                  
+            loop(aggiungi(dummy1, Servizi));             
+        d2 ->                                  
+            loop(aggiungi(dummy2, Servizi));                         
+        tot ->                                                     
+            io:format("servizi chiamati : ~p~n",[Servizi]),              
+            loop(Servizi); %ritorna il numero di chiamate che sono state fatte
+        kill -> unregister(server),exit(normal);                   
+        Other ->                                                   
+            io:format("~pnon è una funzione supportata~n",[Other])
+                                                                   
+    end,                                                           
+    loop(Servizi).                                 
+                                                   
+aggiungi(S, []) ->                                 
+    [{S, 1}];                                        
+aggiungi(S, [{S, N} | T]) ->               
+    [{S, N+1} | T];                        
+aggiungi(S, [H | T]) ->                    
+    [H] ++ aggiungi(S, T).                 

91e93dbb4051c8ef04489f333c0f0856f28d0b6e
Federico <91740549+federicovolpe@users.noreply.github.com>
2023-12-02 22:22:10 +0100

    senza l'utilizzo di liste di argomenti

--- Erlang/Seconda_esercitazione/4DistributedReverseString/mp.erl
+++ Erlang/Seconda_esercitazione/4DistributedReverseString/mp.erl
@@ -4,9 +4,10 @@
 % funzione principale, unica funzione da chiamare
 % il processo master e' il processo corrente                                                                                             
 long_reverse_string(S) ->                                                                      
-    Processes = create_processes(10, self()),                                                                
+    Processes = create_processes(10, self()),   
+    io:format("lunghezza della stringa: ~p diviso 10 : ~p~n",[length(S), length(S) div 10]),                                                             
     send(S, Processes, length(S) div 10), % invia un decimo del problema ad ogni processo slave                                                   
-    wait_answers([]).                     % loop per aspettare le risposte                                                                       
+    wait_answers(1).                     % loop per aspettare le risposte                                                                       
                                                                                                
 create_processes(1, Master) ->                                                                 
     spawn(?MODULE, loop_slave, [1, Master]);                                                   
@@ -14,34 +15,31 @@ create_processes(N, Master) ->
     [spawn(?MODULE, loop_slave, [N, Master]) | create_processes(N-1, Master)].                 
                                                                                                
 % invia a tutti i processi in lista una parte del problema ( un decimo)                                                                             
-send(S, [H | T], Lenght) ->                                                                    
-    io:format("invio a ~p il messaggio ~p~n",[H, string:slice(S, 0, Lenght)]),                                                       
+send(S, [H | T], Lenght) ->                                                                                                          
     H ! string:slice(S, 0, Lenght),                                                   
     send(string:slice(S,Lenght,length(S)), T, Lenght);                     
-send(S, H, _) ->                                                                     
+send(S, H, _) ->                                                                    
     H ! S.                                                                 
                                                                           
 % del processo master si occupa di ricevere tutte le 10 soluzioni dagli slave                                                                                                         
-wait_answers(Solutions) ->                                                                                 
-    receive                                                                                                
-        {Answer, Pnum} ->                                                                                  
-            io:format("ricevuta la risposta ~p da ~p~n", [Answer, Pnum]),                                  
-            Length = length([{Pnum, Answer} | Solutions]),                                               
-            if(Length == 10) ->   % se ho ricevuto tutte e 10 le soluzioni                                                                                                         
-                Soluzione = ricomponi([{Pnum, Answer} | Solutions]),                                                                       
-                io:format("la soluzione ricavata: ~p~n", [Soluzione]);                                                                     
-                                                                                                                                           
-            true -> wait_answers([{Pnum, Answer} | Solutions])                                                                             
-            end                                                                                                                            
-    end.                                                                                                                                   
-                                                                                                          
-% ricompone le soluzioni ricevute dal processo                                                                                                                                           
-ricomponi(L) ->                                                                                                                            
-    Sorted = lists:sort(fun ({A, _}, {B, _}) -> A < B end, L), % riordina la lista {num_processo , soluzione} in base ai numeri di processo    
-    lists:foldl(fun ({_, S}, Acc) -> Acc ++ S end,"", Sorted). % estrapola solo le soluzioni dalla lista precedente e le unisce                                                                           
-                                                                                                          
+wait_answers(N) ->
+    receive
+        {Answer, N} when N == 1->
+            io:format("master riceve ~p da ~p~n", [Answer,N]),
+            Answer ++ wait_answers(N + 1);
+        {Answer, N} when N == 10 ->
+            io:format("master riceve ~p da ~p~n", [Answer,N]),
+            Answer;
+        {Answer, N} ->
+            io:format("master riceve ~p da ~p~n", [Answer,N]),
+            Answer ++ wait_answers(N + 1)
+    end.
+                                                                                                                                
+                                                                                            
 % tiene memorizzato il suo numero progressivo di processo e il master a cui inviare la soluzione elaborata                                                                                                                                          
 loop_slave(N, Master) ->                                                                                                                   
-    receive                                                                                                                                
-        Msg -> Master ! {string:reverse(Msg), N}                                                                                           
+    receive       
+                                                                                                                              
+        Msg ->  io:format("il processo ~p riceve ~p e risponde ~p~n",[self(), Msg, {string:reverse(Msg), N}]),   
+               Master ! {string:reverse(Msg), N}                                                                                           
     end.                                                                                                                                   
